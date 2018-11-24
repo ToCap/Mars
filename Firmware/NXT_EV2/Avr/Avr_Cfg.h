9f6f38a407d7d9679116484d99f9b292bc310d0e
ThomTheCat <thomas.barbillon@gmail.com>
2018-11-24 16:07:19 +0100

    Add Avr feature for NXT_EV2 custom firmware

---
+++ Firmware/NXT_EV2/Avr/Avr_Cfg.h
@@ -0,0 +1,29 @@
+/*
+ ***************************************************************************************************
+ * MARS - the open source AR platform for Mindstorm https://github.com/ToCap/Mars
+ *
+ * Copyright (C) 2020  GC-MS Software (ThomTheCat)
+ *
+ * This source code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation; See <http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>.
+ *
+ *  Software distributed under the License is distributed on an "AS IS"
+ *  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing rights and limitations
+ *  under the License.
+ ***************************************************************************************************
+*/
+
+#ifndef AVR_CFG_H
+#define AVR_CFG_H
+
+/*
+ ***************************************************************************************************
+ * Constants
+ ***************************************************************************************************
+*/
+#define AVR_DEV_ERROR_DETECT			STD_ON
+
+
+#endif /*AVR_CFG_H*/
